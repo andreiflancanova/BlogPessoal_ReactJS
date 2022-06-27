a5479d3875966d6365d92dec5b45c21199ea1bfe
unknown <andreiflancanova@hotmail.com>
2022-06-27 15:24:47 -0300

    FEAT: Adding first React components

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+function Home(){
+    return(
+        <>
+            <h1 className="Titulo">Home</h1>
+            <img src = 
+            "https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGRldmVsb3BlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+             alt ="Dev Background"
+             className="img" />
+        </>
+    );
+}
+
+export default Home;
