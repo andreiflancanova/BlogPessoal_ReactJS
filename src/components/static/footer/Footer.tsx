bf8e64b81e6de0e9a5e9d669143a5bed51483c32
unknown <andreiflancanova@hotmail.com>
2022-06-30 01:12:28 -0300

    REFACTOR: Adding external css files to Footer and Login

--- src/components/static/footer/Footer.tsx
+++ src/components/static/footer/Footer.tsx
@@ -11,28 +11,28 @@ function Footer() {
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#2e86de", height: "130px" }}>
+                    <Box className='box1'>
                         <Box display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" style={{ color: "white" }}>Follow us!</Typography>
+                            <Typography variant="h5" align="center" className='textFormatting'>Follow us!</Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://www.facebook.com/generationbrasil" target="_blank" rel="noreferrer">
-                                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
+                                <FacebookIcon className='social' />
                             </a>
                             <a href="https://www.instagram.com/generationbrasil/" target="_blank" rel="noreferrer">
-                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
+                                <InstagramIcon className='social' />
                             </a>
                             <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank" rel="noreferrer">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+                                <LinkedInIcon className='social' />
                             </a>
                         </Box>
                         <Box display="flex"  justifyContent="center">
                             <img src="https://i.imgur.com/oGKvCzi.png" alt="Logo Generation" height="40px"/>
                         </Box>
                     </Box>
-                    <Box display="flex" justifyContent="center" alignItems="center" style={{ backgroundColor: "#2e86de", height: "40px" }}>
+                    <Box display="flex" justifyContent="center" alignItems="center" >
                         <Box>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2022 Copyright:</Typography>
+                            <Typography variant="subtitle2" align="center" gutterBottom className='textFormatting'>© 2022 Copyright:</Typography>
                         </Box>
                     </Box>
                 </Grid>
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -10,7 +10,7 @@ function Login() {
             <Grid xs={6}>
                 <Box paddingX={20}>
                     <form>
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ fontWeight: 'bold' }}>Entrar</Typography>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='texts'>Entrar</Typography>
                         <TextField id='Usuário' label='Usuário' variant='outlined' name='User' margin='normal' fullWidth />
                         <TextField id='Senha' label='Senha' variant='outlined' name='Password' margin='normal' type='password' fullWidth />
                         <Box marginTop={2} textAlign='center'>
@@ -25,14 +25,11 @@ function Login() {
                         <Box marginRight={1}>
                             <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
                         </Box>
-                        <Typography variant='subtitle1' gutterBottom align='center' style={{fontWeight:'bold'}}>Cadastre-se</Typography>
+                        <Typography variant='subtitle1' gutterBottom align='center' className='texts'>Cadastre-se</Typography>
                     </Box>
                 </Box>
             </Grid>
-            <Grid xs={6} style={{
-                backgroundImage:`url(https://foyr.com/learn/wp-content/uploads/2021/08/modern-office-design.png)`,
-                backgroundRepeat:'no-repeat',width:'100vh',minHeight:'100vh',backgroundSize:'cover',backgroundPosition:'center'
-            }}>
+            <Grid xs={6} className='background'>
 
 
             </Grid>
