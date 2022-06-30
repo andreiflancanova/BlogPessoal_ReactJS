8601878e52f3b9df1d13799416f996447173cb0a
unknown <andreiflancanova@hotmail.com>
2022-06-30 00:41:09 -0300

    REFACTOR: Adding external Home.css file and style improvements

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,22 +6,22 @@ import "./Home.css";
 function Home() {
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "white" }}>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className="GridStyling">
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "black", fontWeight: "bold" }}>Welcome!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "black", fontWeight: "bold" }}>Share your thoughts and express your opinion!</Typography>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className="TypographyStyling">Welcome!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className="TypographyStyling">Share your thoughts and express your opinion!</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
                         </Box>
-                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#2e86de", color: "white" }}>See posts</Button>
+                        <Button variant="outlined" className="Button">See posts</Button>
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
                     <img src="https://cdn.create.vista.com/api/media/medium/221706396/stock-photo-cup-coffee-laptop-program-code?token=" alt="" width="100%" height="500px" />
                 </Grid>
-                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                <Grid xs={12} className="Postagens">
                 </Grid>
             </Grid>
         </>
