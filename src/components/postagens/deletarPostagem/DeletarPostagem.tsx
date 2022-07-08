a325c19a153e7ccb7159ffafa8383d04fdd4058b
unknown <andreiflancanova@hotmail.com>
2022-07-07 23:36:52 -0300

    FEAT(34): Adding Delete Postagem

--- src/components/postagens/deletarPostagem/DeletarPostagem.tsx
+++ src/components/postagens/deletarPostagem/DeletarPostagem.tsx
@@ -3,9 +3,55 @@ import {Typography, Card, CardActions, CardContent } from "@material-ui/core"
 import './DeletarPostagem.css';
 import {Box, Button} from '@mui/material';
 import Postagem from '../../../models/Postagem';
+import { useNavigate, useParams } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { buscaId, deleteId } from '../../../services/Service';
 
 function DeletarPostagem() {
-   
+  let navigate = useNavigate();
+  const { id } = useParams<{id: string}>();
+  const [token, setToken] = useLocalStorage('token');
+  const [post, setPosts] = useState<Postagem>()
+
+  useEffect(() => {
+      if (token == "") {
+          alert("Você precisa estar logado")
+          navigate("/login")
+  
+      }
+  }, [token])
+
+  useEffect(() =>{
+      if(id !== undefined){
+          findById(id)
+      }
+  }, [id])
+
+  async function findById(id: string) {
+      buscaId(`/postagens/${id}`, setPosts, {
+          headers: {
+            'Authorization': token
+          }
+        })
+      }
+
+      function sim() {
+        //Rota do Front-End
+          navigate('/posts')
+
+        //Rota do Back-End  
+          deleteId(`/postagens/${id}`, {
+            headers: {
+              'Authorization': token
+            }
+          });
+          alert('Postagem deletada com sucesso');
+        }
+      
+        function nao() {
+          navigate('/posts')
+        }
+        
   return (
     <>
       <Box m={2}>
@@ -16,7 +62,7 @@ function DeletarPostagem() {
                 Deseja deletar a Postagem:
               </Typography>
               <Typography color="textSecondary" >
-              Tema
+              {post?.titulo}
               </Typography>
             </Box>
 
@@ -24,12 +70,12 @@ function DeletarPostagem() {
           <CardActions>
             <Box display="flex" justifyContent="start" ml={1.0} mb={2} >
               <Box mx={2}>
-              <Button  variant="contained" className="marginLeft" size='large' color="primary">
+              <Button onClick={sim} variant="contained" className="marginLeft" size='large' color="primary">
                 Sim
               </Button>
               </Box>
               <Box>
-              <Button   variant="contained" size='large' color="secondary">
+              <Button onClick={nao} variant="contained" size='large' color="secondary">
                 Não
               </Button>
               </Box>
